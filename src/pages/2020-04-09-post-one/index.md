c4a757b0a1a0bb8540a31fee5db6d82523c69bd8
Miguel Angel <miguelapb35@gmail.com>
2020-07-18 07:08:56 -0500

    first commit

---
+++ src/pages/2020-04-09-post-one/index.md
@@ -0,0 +1,9 @@
+---
+path: "/post-one"
+date: "2020-04-09"
+title: "My First Gatsby Post"
+author: "Miguel Parra"
+---
+
+This is my very first blog 
+post in Gatsby
---
+++ src/pages/services.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const ServicesPage = () => 
+    <Layout>
+      <div>
+        <h1>Services</h1>
+        <p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, perspiciatis. Quas debitis accusamus eveniet non ratione aut quod atque harum voluptatem illo velit blanditiis excepturi architecto mollitia hic, tenetur impedit sed provident iure dolore! Laudantium neque debitis voluptatem? Quos, nostrum.
+        </p>
+      </div>
+    </Layout>
+
+
+export default ServicesPage
